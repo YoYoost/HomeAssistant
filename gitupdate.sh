2eb14e3358b2c3b4160b010075ae1ce9a4d167d3
YoYoost <yoyoost@hotmail.com>
2018-01-16 10:53:41 +0100

    Initial commit of my Home Assistant configuration. Added README, gitupdate.sh, Travis CI, Spotify and DuckDNS.

---
+++ gitupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#sudo su -s /bin/bash homeassistant
+#source /srv/homeassistant/bin/activate
+#hass --script check_config
+
+cd /home/homeassistant/.homeassistant
+
+git add .
+git status
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
+
+exit
